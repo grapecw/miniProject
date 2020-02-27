a7e236b904791ca69a68fee97ca32dc63b606389
me <overelectric@gmail.com>
2020-02-27 20:15:59 +0900

    네이게이션바 분리

--- src/main/java/my/spring/miniproject/HomeController.java
+++ src/main/java/my/spring/miniproject/HomeController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartRequest;
 import org.springframework.web.servlet.ModelAndView;
@@ -102,20 +103,22 @@ public class HomeController {
 	@Autowired
 	ServletContext context; 
 	
-	@RequestMapping("/image")
+	@RequestMapping(value = "/image", produces = "application/json; charset=utf-8")
+	@ResponseBody
 	public ModelAndView saveFile(MultipartRequest mreq) {
-		System.out.println("들어왔니?");
+//		System.out.println("들어왔니?");
+		String fileInfo=null;
 		ModelAndView mav = new ModelAndView();
 		List<MultipartFile> list = mreq.getFiles("file");
 		String resultStr = "";
 		mav.setViewName("boardWrite");
-		System.out.println("여긴 옴??");
+//		System.out.println("여긴 옴??");
 		for (MultipartFile mfile : list) {
-			System.out.println("여기는??");
+//			System.out.println("여기는??");
 			String fileName = mfile.getOriginalFilename();
 			try {
-				System.out.println(context.getRealPath("/"));
-				String fileInfo = context.getRealPath("/") + "resources/img/"+fileName;
+//				System.out.println(context.getRealPath("/"));
+				fileInfo = context.getRealPath("/") + "resources/img/"+fileName;
 				// context.getRealPath 而⑤뱶濡ㅻ윭, 利� �씠 �봽濡쒖젥�듃�쓽 理쒖긽�엳 �뤃�뜑瑜� 異붿텧�븯�뒗 臾몄옣
 				// �듅�젙 �쐞移섏뿉 �삱由щ㈃ �겢�씪�씠�뼵�듃媛� �젒洹쇱쓣 �븯吏� 紐삵븳, 洹몃윭�땲源� �겢�씪�씠�뼵�듃媛� �젒洹� 媛��뒫�븳 �뤃�뜑�씤 resources�뿉 �삱由ш린 �쐞�빐�꽌
 				File f = new File(fileInfo);
@@ -132,7 +135,9 @@ public class HomeController {
 				resultStr += fileName + " : �삤瑜섍� 諛쒖깮�뻽�뼱�슂!!";				
 			}
 		}
-		mav.addObject("msg", resultStr);	
+		mav.addObject("url", resultStr);
+//		System.out.print(fileInfo);
+		String s = "{\"url\" : \""+fileInfo+"\"}";
 		return mav;
 	}
 	
--- src/main/webapp/WEB-INF/views/uploadboard.jsp
+++ src/main/webapp/WEB-INF/views/uploadboard.jsp
@@ -3,7 +3,7 @@
 	import="java.util.List, java.util.ArrayList,vo.ProdReviewVO"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
 
 <meta charset="utf-8">
